c6c0f82709f2dacec81118df3a7456c2f60ca6d0
Nigel <mail@nigelmoes.nl>
2024-03-12 15:56:58 -0500

    Added code that creates a Java Object and saves it in a MySQL database using Hibernate

--- src/main/java/com/nigel/HibernateDemo/HibernateDemoApplication.java
+++ src/main/java/com/nigel/HibernateDemo/HibernateDemoApplication.java
@@ -1,5 +1,7 @@
 package com.nigel.HibernateDemo;
 
+import com.nigel.HibernateDemo.dao.StudentDAO;
+import com.nigel.HibernateDemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,8 +15,22 @@ public class HibernateDemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner ->
-				System.out.println("Hello World!");
+				createStudent(studentDAO);
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+
+		// create the student object
+		System.out.println("Creating new student object...");
+		Student tempStudent = new Student("Nigel", "Moes", "nigel@moes.com");
+
+		// save the student object
+		System.out.println("Saving the student...");
+		studentDAO.save(tempStudent);
+
+		// display id of the saved student
+		System.out.println("Saved student. Generated id: " + tempStudent.getId());
 	}
 }
---
+++ src/main/java/com/nigel/HibernateDemo/dao/StudentDAO.java
@@ -0,0 +1,8 @@
+package com.nigel.HibernateDemo.dao;
+
+import com.nigel.HibernateDemo.entity.Student;
+
+public interface StudentDAO {
+
+    void save(Student theStudent);
+}
